f06df7735792886a3ab1084461ab4b65bd24e01f
rin3desu <rinsei3110@gmail.com>
2025-07-27 17:12:02 +0900

    体重の推移を可視化できるようにした

--- app.py
+++ app.py
@@ -24,14 +24,25 @@ init_db()  #アプリ起動時に1度だけ呼び出す
 def index():
     weight = None
     date = None
+    
     if request.method == "POST":
         weight = request.form.get("weight")
-        date = request.form.get("date")
-        if weight and date:
-            with sqlite3.connect('diet.db') as conn:    
-                cur = conn.cursor()
-                cur.execute("INSERT INTO weights (date, weight) VALUES (?, ?)", (date,weight))
-                conn.commit()
+        date = request.form.get("date") 
+        
+        conn = sqlite3.connect('diet.db')
+        cur = conn.cursor()
+        cur.execute("INSERT INTO weights (date, weight) VALUES (?, ?)", (date,weight))
+        conn.commit()
+        conn.close()
+        
+    #全データを取得してグラフに渡す
+    conn = sqlite3.connect('diet.db')
+    cur = conn.cursor()
+    cur.execute("SELECT date,weight FROM weights ORDER BY date")
+    data = cur.fetchall()
+    conn.close()
+    dates = [row[0] for row in data]
+    weights = [row[1] for row in data]
     
     #ここで履歴を取得（新しい順）            
     with sqlite3.connect('diet.db') as conn:
@@ -39,7 +50,7 @@ def index():
         cur.execute("SELECT date, weight FROM weights ORDER BY date DESC")
         records = cur.fetchall()
         
-    return render_template("index.html",weight = weight, date = date, records = records)  # templates/index.htmlをブラウザに返す
+    return render_template("index.html",weight = weight, date = date,dates=dates,weights =weights ,records = records)  # templates/index.htmlをブラウザに返す
 
 if __name__ == "__main__":  # このファイルが直接実行された場合に以下を実行
     app.run(debug=True)  # 開発用サーバーを起動。debug=Trueで自動リロードや詳細エラー表示
